04f620f648e3b0e5b1ba5117a8c489573039573a
fellobello <scochrane02@gmail.com>
2025-11-04 10:47:36 -0800

    Bazel setup with C++20

---
+++ src/gesture_capture.h
@@ -0,0 +1,10 @@
+#ifndef GESTURE_CAPTURE_H
+#define GESTURE_CAPTURE_H
+
+namespace gesture {
+
+void start_capture();
+
+}
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include "gesture_capture.h"
+
+int main() {
+	gesture::start_capture();
+	return 0;
+}
